c0b565174f9d225a8f23c9a245e7a2f39c4636e7
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-09-22 20:03:59 +0900

    パッケージ更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from 'vite-plugin-vue-devtools';
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   // https://vitejs.dev/config/
   return defineConfig({
     base: process.env.VITE_APP_BASE_PATH,
     plugins: [
       vue(),
+      vueDevTools(),
       tsconfigPaths(),
     ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
     server: {
       proxy: {
         "/api": {
